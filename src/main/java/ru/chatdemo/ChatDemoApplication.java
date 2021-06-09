aa5310bd6abbc7e1605a22fc446bde9033a9b155
DmitryShvyryaev <DmitryShvuryaev@yandex.ru>
2021-06-09 14:28:50 +0300

    styles_css

--- src/main/java/ru/chatdemo/ChatDemoApplication.java
+++ src/main/java/ru/chatdemo/ChatDemoApplication.java
@@ -12,19 +12,8 @@ import java.time.LocalDate;
 
 @SpringBootApplication
 @AllArgsConstructor
-public class ChatDemoApplication implements ApplicationRunner {
-
-    private final MessageRepository repository;
-
+public class ChatDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatDemoApplication.class, args);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        repository.save(new Message("Dmitry", LocalDate.now().atTime(11,0), "Heelo brothers"));
-        repository.save(new Message("Alexy", LocalDate.now().atTime(9,36), "Hey Im OK"));
-        repository.save(new Message("Dmitry", LocalDate.now().minusDays(1).atTime(11,0), "And you"));
-        repository.save(new Message("Alexy", LocalDate.now().minusDays(3).atTime(11,0),"Heelo brothers"));
     }
 }
